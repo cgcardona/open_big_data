3b1664237458fd9ad0e6d689fd5bbad8a7fcc414
Carlos Cardona <cgcardona@gmail.com>
2013-02-01 01:02:06 -0800

    stub rails app with haml and underscore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpenBigData::Application.config.secret_token = '42c087f7f52d0c8cac2a0d63d5adab985842d2beb311f9569723d29d3b1bd82512b6119e1d1a67bb43c905005f60ee5bdd3c393043d3b53f6bb3a9009a5d962e'
